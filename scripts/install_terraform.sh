f1a652bb48de9110bdb58ac69c2811a1cccb9fc7
adam.pelle <adam.pelle@bitrise.io>
2021-05-14 13:39:01 +0200

    check checksum of install script

--- scripts/install_terraform.sh
+++ scripts/install_terraform.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+FILE="tfswitch-install.sh"
+INSTALL_SCRIPT_COMMIT="2865ec9eb81fa8dc8cac1804d9c946999e1a785e"
+MD5_CHECKSUM="db8a16f4fb9cd1e6cc728ac84754b0e7  $FILE"
+
 # install tfswitch
-curl -sSL https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
+curl -sSL https://raw.githubusercontent.com/warrensbox/terraform-switcher/$INSTALL_SCRIPT_COMMIT/install.sh -o $FILE
+
+if [[ "$(md5sum $FILE)" != $MD5_CHECKSUM ]]; then
+exit 1
+fi
+
+# make the script executable
+chmod 755 $FILE
+
+# install tfswitch
+./$FILE -b /usr/local/bin
 
 # install terraform
 tfswitch $TERRAFORM_VERSION
+
+# remove installer script
+rm $FILE
