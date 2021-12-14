926fe8c08767f2e680c2f44dfd6513d3b7ad16c1
zsolt-marta-bitrise <zsolt.marta@bitrise.io>
2021-12-14 10:17:18 +0100

    feat: TIP-92 Add support for a cluster hosted in a different GCP project (#8)

--- step/main.sh
+++ step/main.sh
@@ -6,15 +6,17 @@ WD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ ! -z "$GCLOUD_KEY" ]; then
 echo -n $GCLOUD_KEY > /tmp/gcloud_key.json
 
-GCLOUD_PROJECT=$(echo -n $GCLOUD_KEY | jq -r '.project_id')
+GCLOUD_SA_PROJECT=$(echo -n $GCLOUD_KEY | jq -r '.project_id')
+if [ -z "$GKE_CLUSTER_PROJECT" ]; then
+GKE_CLUSTER_PROJECT=$GCLOUD_SA_PROJECT
+fi
 GCLOUD_USER=$(echo -n $GCLOUD_KEY | jq -r '.client_email')
 
-echo "Authenticating $GCLOUD_USER to $GCLOUD_PROJECT project..."
+echo "Authenticating $GCLOUD_USER to $GCLOUD_SA_PROJECT project..."
 
 export GOOGLE_APPLICATION_CREDENTIALS=/tmp/gcloud_key.json
 
 gcloud auth activate-service-account $GCLOUD_USER --key-file=/tmp/gcloud_key.json
-gcloud config set project $GCLOUD_PROJECT
 
 for REG in "gcr.io" "us.gcr.io"; do
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://$REG
@@ -26,9 +28,8 @@ if [ ! -z "$GKE_CLUSTER" ]; then
 echo "Setting up access to $GKE_CLUSTER..."
 
 # setup kubectl access
-gcloud container clusters get-credentials --region=$GKE_CLUSTER_REGION $GKE_CLUSTER
+gcloud container clusters get-credentials --project=$GKE_CLUSTER_PROJECT --region=$GKE_CLUSTER_REGION $GKE_CLUSTER
 fi
-
 
 if [ ! -z "$TERRAFORM_DIR" ]; then
 echo "Initializing terraform in $TERRAFORM_DIR... using $TERRAFORM_WORKSPACE"
@@ -62,11 +63,5 @@ echo "You need to specify TERRAFORM_DIR input with TERRAFORM_SECRETS"
 fi
 
 # Outputs
-if [ ! -z "$GCLOUD_PROJECT" ]; then
-SERVICE_IMAGE_ID="gcr.io/$GCLOUD_PROJECT/$SERVICE_NAME:$BITRISE_GIT_COMMIT"
-fi
-
-envman add --key SERVICE_IMAGE_ID --value "$SERVICE_IMAGE_ID"
-envman add --key GCLOUD_PROJECT --value "$GCLOUD_PROJECT"
 envman add --key GCLOUD_USER --value "$GCLOUD_USER"
 envman add --key GOOGLE_APPLICATION_CREDENTIALS --value "$GOOGLE_APPLICATION_CREDENTIALS"
